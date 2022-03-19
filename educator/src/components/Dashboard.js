4e4a6103099f98444538637bbf1889335809e4da
Robert Oles <spamcislo2@hotmail.sk>
2022-03-19 14:27:36 +0100

    Edit initial page

--- educator/src/components/Dashboard.js
+++ educator/src/components/Dashboard.js
@@ -1,16 +1,19 @@
 const Dashboard = function () {
   return (
-    <ul>
-      <li>
-        <a href="./SocketNav">Socket Navigation</a>
-      </li>
-      <li>
-        <a href="./S2t">Speech to text</a>
-      </li>
-      <li>
-        <a href="./Recording">Recording</a>
-      </li>
-    </ul>
+    <div>
+      <h1>This is the initial page. Navigate to:</h1>
+      <ul>
+        <li>
+          <a href="./SocketNav">Socket Navigation</a>
+        </li>
+        <li>
+          <a href="./S2t">Speech to text</a>
+        </li>
+        <li>
+          <a href="./Recording">Recording</a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
