7717612a47eae2f316ce9e05d208ad0e2cfbbcb7
Robert Oles <spamcislo2@hotmail.sk>
2022-04-01 19:38:34 +0200

    Remove some unnecesary components

--- educator/src/App.js
+++ educator/src/App.js
@@ -1,12 +1,4 @@
 import "./App.css";
-import Stocks from "./components/Stocks";
-import Obligations from "./components/Obligations";
-import Risk from "./components/Risk";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SocketNavigation from "./components/SocketNavigation";
-import Recording from "./components/Recording";
-import sttFromMic from "./components/S2t";
-import Dashboard from "./components/Dashboard";
 import EducatorButton from "./components/EducatorButton";
 
 function App() {
