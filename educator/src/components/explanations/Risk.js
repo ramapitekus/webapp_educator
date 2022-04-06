1bd3e2ef9ef5dfc505ed90845ad09540a8550455
Robert Oles <spamcislo2@hotmail.sk>
2022-04-06 20:47:49 +0200

    Add video explanation

--- educator/src/components/explanations/Risk.js
+++ educator/src/components/explanations/Risk.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./explanations.css";
 
 const Risk = ({ onClose }) => {
+  const vidRef = useRef();
+
+  useEffect(() => {
+    vidRef.current.play();
+  }, []);
+
   return (
     <>
       <div className="modalStyles" />
       <div className="overlayStyles">
-        <button className="button buttonEndExplanation" onClick={onClose}>
+        <button className="button buttonEndExplanationVideo" onClick={onClose}>
           End Explanation
         </button>
-        Risk explanation page
+        <video src="/clips/Understanding Risks.mp4" ref={vidRef} autoPlay />
       </div>
     </>
   );
